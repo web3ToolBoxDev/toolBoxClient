699933e60aeb2a8286ab0ad9da7119ce197d0e81
Aming <yingzhang@yingdeMacBook-Pro.local>
2024-04-08 11:22:32 +0800

    修改介绍页

--- client/src/pages/introduction/index.js
+++ client/src/pages/introduction/index.js
@@ -64,7 +64,7 @@ const Introduction = () => {
       </Row>
       <Row style={{ border: '1px solid black',margin:'5px' }}>
         <Col md={2} style={{ fontSize: '1.5vw', borderRight: '1px solid black' }}>
-          V0.1.3新增IP代理功能
+          V0.1.3新增
         </Col>
         <Col md={8} style={{ fontSize: '1.5vw', borderRight: '1px solid black' }}>
           实现socks5与http的IP代理
